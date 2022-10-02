796d0d52790ad135f19c30ecffc5b8dfb2f8bd61
caseyhxynh <114814385+caseyhxynh@users.noreply.github.com>
2022-10-01 21:01:55 -0400

    Add files via upload

---
+++ MadlibGithub.py
@@ -0,0 +1,23 @@
+#Madlib fill in the blanks about my interests using inputs, strings, and variables
+
+
+from re import L
+
+
+university_name = input("Enter a University Name: ")
+major = input("Enter your major: ")
+extra_hobby = input("Enter a hobby: ")
+game_name = input("Enter a game name: ")
+country_name = input("Enter country name:  ")
+music_genre = input("Enter favorite music genre: ")
+song_name = input("Enter favorite song: ")
+artist_name = input("Enter artist of favorite song: ")
+car_name = input("Enter dream car: ")
+
+
+
+
+
+print("Hi, My name is Casey, I am a student at " + university_name + ". I am studying " + major + " there! There are many things that interest me. Some of my hobbies include playing games, traveling, listening to music, and " 
++ extra_hobby + ".\nMy favorite game to play is " + game_name + "." + " The best country that I've traveled to is " + country_name + "." + " Music is a must for me. My favorite genre of music is "
++ music_genre + "." + " My favorite song at the moment is " + song_name + " by " + artist_name + "." + "\nI also am really into cars. My dream car is a " + car_name + "." + " Thats it for this madlib! Thanks for checking it out!")
